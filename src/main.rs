46f2782a67b21a9fa31a0053f74342bae27979d0
Ben Allison <ben@checkfront.com>
2017-05-23 03:54:06 -0700

    Added -d decode mode, added [limit] for generating multiple options

--- src/main.rs
+++ src/main.rs
@@ -17,41 +17,88 @@ fn get_file_lines_iter(name: &str) -> std::io::Lines<std::io::BufReader<std::fs:
     let adj_reader = BufReader::new(adj_file);
     return adj_reader.lines();
 }
+macro_rules! print_return {
+    ($x:expr) => {
+        println!($x);
+        return;
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.contains(&("-h".to_owned())) {
+        print_return!("Usage:\
+            \n    brute-longer target max [limit]\
+            \n    brute-longer -d adjective noun max");
+    }
+    if args.contains(&("-d".to_owned())) {
+        main_decode_words(args);
+    } else {
+        main_brute_words(args);
+    }
+}
+
+fn main_brute_words(args: Vec<String>) {
+
     if args.len() < 3 {
-        println!("Usage: brute-longer [target] [max]");
-        return;
+        print_return!("Usage: brute-longer target max [limit]");
     }
     let target: u64 = match args[1].parse() {
         Err(_) => {
-            println!("Usage: brute-longer [target] [max]");
-            return;
+            print_return!("Usage: brute-longer target max [limit]");
         },
-        Ok(target) => target,
+        Ok(ok) => ok,
     };
     let max: u64 = match args[2].parse() {
         Err(_) => {
-            println!("Usage: brute-longer [target] [max]");
-            return;
+            print_return!("Usage: brute-longer target max [limit]");
         },
-        Ok(max) => max,
+        Ok(ok) => ok,
     };
     if target >= max {
-        println!("target must be < max!");
-        return;
+        print_return!("target must be < max!");
     }
+    let mut limit: u8 = 1;
+    if args.len() >= 4 {
+        limit = match args[3].parse() {
+            Err(_) => {
+                print_return!("Usage: brute-longer target max [limit]");
+            },
+            Ok(ok) => ok,
+        }
+    }
+    brute_words_from_int(target, max, limit);
+}
 
+
+fn main_decode_words(args: Vec<String>) {
+    if args.len() < 5 {
+        print_return!("Usage: brute-longer -d adjective noun max");
+    }
+    let ref adj = args[2];
+    let ref noun = args[3];
+    let max: u64 = match args[4].parse() {
+        Err(_) => {
+            print_return!("Usage: brute-longer -d adjective noun max");
+        },
+        Ok(ok) => ok,
+    };
+
+    let mut hasher = FnvHasher::default();
+    adj.hash(&mut hasher);
+    noun.hash(&mut hasher);
+    let hash = hasher.finish();
+    let target = hash % max;
+    println!("{}", target);
+}
+
+fn brute_words_from_int(target: u64, max: u64, mut limit: u8) {
     let mut adjs = get_file_lines_iter("words/adjectives.txt");
     let mut nouns = get_file_lines_iter("words/nouns.txt");
 
-    let mut hasher = FnvHasher::default();
-    let mut num_hashed = 0;
     loop {
-        num_hashed += 1;
-        let adj = match adjs.next() {
+        let adj : std::string::String = match adjs.next() {
             None => {
                 adjs = get_file_lines_iter("words/adjectives.txt");
                 continue;
@@ -61,7 +108,7 @@ fn main() {
                 Ok(y) => y,
             },
         };
-        let noun = match nouns.next() {
+        let noun : std::string::String = match nouns.next() {
             None => {
                 nouns = get_file_lines_iter("words/nouns.txt");
                 continue;
@@ -71,13 +118,18 @@ fn main() {
                 Ok(y) => y,
             },
         };
+        let mut hasher = FnvHasher::default();
         adj.hash(&mut hasher);
         noun.hash(&mut hasher);
 
         let hash = hasher.finish();
         if (hash % max) == target {
-            println!("Found: {} {} after {} attempts", adj, noun, num_hashed);
-            break;
+            println!("{} {}", adj, noun);
+            if limit > 1 {
+                limit -= 1;
+            } else {
+                break;
+            }
         }
 
     }
